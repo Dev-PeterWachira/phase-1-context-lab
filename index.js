0168a6068b30b91f3029db9d17a7be02ffc69215
Dev-PeterWachira <wmainap@gmail.com>
2024-10-18 08:36:46 +0300

    context in js

--- index.js
+++ index.js
@@ -9,7 +9,85 @@
  for you to use if you need it!
  */
 
-const allWagesFor = function () {
+ function createEmployeeRecord(arr) {
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+function createEmployeeRecords(arrays) {
+    return arrays.map(arr => createEmployeeRecord(arr));
+}
+
+function createTimeInEvent(dateString) {
+    const [date, hour] = dateString.split(" ");
+    const timeInEvent = {
+        type: "TimeIn",
+        date: date,
+        hour: parseInt(hour)
+    };
+    this.timeInEvents.push(timeInEvent);
+    return this;
+}
+
+function createTimeOutEvent(dateString) {
+    const [date, hour] = dateString.split(" ");
+    const timeOutEvent = {
+        type: "TimeOut",
+        date: date,
+        hour: parseInt(hour)
+    };
+    this.timeOutEvents.push(timeOutEvent);
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(e => e.date === date);
+    const timeOut = this.timeOutEvents.find(e => e.date === date);
+    return (timeOut.hour - timeIn.hour) / 100
+}
+
+function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date);
+    return hoursWorked * this.payPerHour;
+}
+
+function allWagesFor(employee) {
+    if (!employee || !employee.timeInEvents) {
+        return 0; 
+    }
+    const dates = employee.timeInEvents.map(event => event.date);
+    return dates.reduce((totalWages, date) => {
+        return totalWages + wagesEarnedOnDate.call(employee, date);
+    }, 0);
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName);
+}
+
+function calculatePayroll(employees) {
+    return employees.reduce((total, employee) => {
+        return total + allWagesFor(employee);
+    }, 0);
+}
+
+function allWagesFor(employee) {
+    const eligibleDates = employee.timeInEvents.map(event => event.date);
+    
+    return eligibleDates.reduce((memo, date) => {
+        return memo + wagesEarnedOnDate(employee, date);
+    }, 0);
+}
+
+
+
+const allWagesForBound = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
